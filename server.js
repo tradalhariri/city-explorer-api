d052805f532004a938ce7e3d17906f1901765e42
tradalhariri <trad.alhariri@gmail.com>
2021-08-16 19:51:46 +0300

    compare also with lan and lon

--- server.js
+++ server.js
@@ -16,10 +16,13 @@ server.get('/',(req,res)=>{
 
 server.get('/weather', (req, res) => {
 try{
-    let cityWeatherData = weatherData.find(city => city.city_name.toLowerCase() === req.query.searchQuery.toLowerCase())
+    let cityWeatherData = weatherData.find(city =>  
+        
+        city.city_name.toLowerCase() === req.query.searchQuery.toLowerCase() && parseInt(city.lat) === parseInt(req.query.lat) && parseInt(city.lon) === parseInt(req.query.lon));
+
     let cityWeatherDataDays = cityWeatherData.data.map(day => {
         let forecastDay = new Forecast(day.datetime, `Low of ${day.low_temp} , High of ${day.max_temp} with  ${day.weather.description}`)
-        
+
         return forecastDay;
     });
     res.send(cityWeatherDataDays);
